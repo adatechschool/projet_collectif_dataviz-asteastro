f3816ca89a979f442f4b273f23141895bebc9b1b
morganelepine <114992081+morganelepine@users.noreply.github.com>
2022-11-21 16:36:21 +0100

    21/11

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//const url = "https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY"
+import fetch from 'node-fetch';
+
+//function fetchData(){
+    fetch("https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY")
+    .then(response => response.json())
+    .then(response2 => console.log(response2)) 
+//}
+//fetchData()
+
+const title = document.querySelector("#title")
+const photo = document.querySelector("#photo")
+const description = document.querySelector("#description")
+
+function displayData(data){
+    title.innerHTML = data.title;
+
+}
+displayData(response2)
