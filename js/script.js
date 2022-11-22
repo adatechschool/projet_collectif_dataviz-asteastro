eece87ccd7b8cadffc1cdf1ed7b2b394f5f2351b
morganelepine <114992081+morganelepine@users.noreply.github.com>
2022-11-22 11:40:19 +0100

    hello la team

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-//const url = "https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY"
 // import fetch from 'node-fetch';
 
-//function fetchData(){
-fetch(
-  "https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY"
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((nasaData) => {
-    console.log(nasaData);
-    const title = document.querySelector("#title");
-    title.innerHTML = nasaData.title;
-    const photo = document.querySelector("#photo");
-    photo.src = nasaData.url;
-    const description = document.querySelector("#description");
-    description.innerHTML = nasaData.explanation;
-  });
-//}
+const url = "https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY&date="
+
+//Afficher l'image lorsqu'une date est sélectionnée
+document.getElementById("date").oninput = function() {fetchData()};
+
+function dateSaisie(){
+  const saisie = document.getElementById("date").value;
+  console.log(saisie);
+  return saisie
+  }
+
+function fetchData(){
+  fetch(url + dateSaisie() + "&")
+    .then((response) => {
+      return response.json();
+    })
+    .then((nasaData) => {
+      console.log(nasaData);
+      const title = document.querySelector("#title");
+      title.innerHTML = nasaData.title;
+      const photo = document.querySelector("#photo");
+      photo.src = nasaData.url;
+      const description = document.querySelector("#description");
+      description.innerHTML = nasaData.explanation;
+    });
+}
 //fetchData()
-
-// const title = document.querySelector("#title");
-// const photo = document.querySelector("#photo");
-// const description = document.querySelector("#description");
-
-// function displayData(data) {
-//   title.innerHTML = data.title;
-// }
-// displayData(response2);
