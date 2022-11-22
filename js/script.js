794322498cdf3b36aa03ec12f9191f9f123cd2ff
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2022-11-22 09:46:50 +0100

    mise a jour sans node
    suppression Node et fonction nasaData

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 //const url = "https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY"
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
 //function fetchData(){
-    fetch("https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY")
-    .then(response => response.json())
-    .then(response2 => console.log(response2)) 
+fetch(
+  "https://api.nasa.gov/planetary/apod?api_key=byMXZRYPDymQvCcgAEarTFUmCZVtf4OXrnRu5UPY"
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((nasaData) => {
+    console.log(nasaData);
+    const title = document.querySelector("#title");
+    title.innerHTML = nasaData.title;
+    const photo = document.querySelector("#photo");
+    photo.src = nasaData.url;
+    const description = document.querySelector("#description");
+    description.innerHTML = nasaData.explanation;
+  });
 //}
 //fetchData()
 
-const title = document.querySelector("#title")
-const photo = document.querySelector("#photo")
-const description = document.querySelector("#description")
+// const title = document.querySelector("#title");
+// const photo = document.querySelector("#photo");
+// const description = document.querySelector("#description");
 
-function displayData(data){
-    title.innerHTML = data.title;
-
-}
-displayData(response2)
+// function displayData(data) {
+//   title.innerHTML = data.title;
+// }
+// displayData(response2);
